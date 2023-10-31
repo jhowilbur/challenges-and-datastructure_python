0e2f5812e02fdcd8bd828e205467eb4b5d66527f
Wilbur <wilbur@wilbur.com>
2023-10-31 07:40:30 -0400

    - improve linkedlist class

--- algo/linked_list/linked_list.py
+++ algo/linked_list/linked_list.py
@@ -6,30 +6,58 @@ class Node:
 
 class LinkedList:
     def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
+        self.head = Node(value)
+        self.tail = self.head
         self.length = 1
 
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
+    def print_value(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value)
+            current_node = current_node.next
 
     def append(self, value):
         new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
+        self.tail.next = new_node
+        self.tail = new_node
         self.length += 1
         return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+
+        pre_node = self.head
+        selected_node = self.head
+
+        while selected_node.next:
+            pre_node = selected_node
+            selected_node = selected_node.next
+        self.tail = pre_node
+        self.tail.next = None
+        self.length -= 1
+
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+
+        return pre_node
+
+    def find_middle(self):
+        slow_pointer = self.head
+        fast_pointer = self.head
+
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
 
 
 my_ll = LinkedList(4)
 my_ll.append(5)
-my_ll.print_list()
+my_ll.append(7)
+my_ll.append(8)
+# my_ll.print_value()
 
+print(my_ll.find_middle().value)
