11cafa5df0bac2e391cfc187382655e3af898294
Wilbur <wilbur@wilbur.com>
2023-11-03 04:36:51 -0400

    - updates in binary tree algorithm

--- algo/graph/binary_tree.py
+++ algo/graph/binary_tree.py
@@ -1,3 +1,15 @@
+from functools import wraps
+
+
+def default_node(f):  # decorator to avoid all the time use if node is None in the methods
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        if 'node' not in kwargs or kwargs['node'] is None:
+            kwargs['node'] = self.root
+        return f(self, *args, **kwargs)
+    return wrapper
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -9,29 +21,51 @@ class Node:
 class BinaryTree:
     def __init__(self, value):
         new_node = Node(value)
-        self.ancestor = None
         self.root = new_node
 
-    def append(self, value, node):
+    @default_node
+    def append(self, value, node=None):
         new_node = Node(value)
+        side = 'left_edge' if value <= node.value else 'right_edge'
+        if getattr(node, side) is None:
+            setattr(node, side, new_node)
+        else:
+            self.append(value, node=getattr(node, side))
 
-        if value <= node.value and node.left_edge is None:
-            node.left_edge = new_node
-        elif value > node.value and node.right_edge is None:
-            node.right_edge = new_node
+    @default_node
+    def print_tree(self, node=None):
+        print(node.value)
+        if node.right_edge:
+            self.print_tree(node=node.right_edge)
+        if node.left_edge:
+            self.print_tree(node=node.left_edge)
 
-        elif value <= node.value and node.left_edge is not None:
-            self.append(value, node.left_edge)
-        elif value > node.value and node.right_edge is not None:
-            self.append(value, node.right_edge)
+    # TODO complete the remove method
+    @default_node
+    def remove_big_node(self, node=None):
+        while node.right_edge is not None:
+            actual_node = node
+            node = node.right_edge
+            if node.right_edge is None:
+                actual_node.right_edge = None
+                return actual_node
 
 
 binary_tree = BinaryTree(2)
-binary_tree.append(3, binary_tree.root)
-binary_tree.append(4, binary_tree.root)
-binary_tree.append(5, binary_tree.root)
-binary_tree.append(2, binary_tree.root)
-binary_tree.append(3, binary_tree.root)
-binary_tree.append(3, binary_tree.root)
-binary_tree.append(4, binary_tree.root)
-print(binary_tree)
+# binary_tree.append(3)
+# binary_tree.append(4)
+# binary_tree.append(5)
+# binary_tree.append(2)
+# binary_tree.append(3)
+# binary_tree.append(3)
+# binary_tree.append(4)
+
+binary_tree.append(3)
+binary_tree.append(4)
+binary_tree.append(5)
+
+binary_tree.print_tree()
+
+# binary_tree = binary_tree.remove_big_node()
+#
+# print(binary_tree)
